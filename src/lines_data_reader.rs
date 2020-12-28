e46c83a6e7be50d0ed820474b3dbf46a1d71ac67
Thomas Weber <thomas.weber@notengrafik.com>
2020-12-28 15:48:08 +0100

    Cleanup of parser

--- src/lines_data_reader.rs
+++ src/lines_data_reader.rs
@@ -1,4 +1,4 @@
-use crate::{Layer, Line, Page, Point, Color, BrushType};
+use crate::{BrushType, Color, Layer, Line, Page, Point};
 use byteorder::{LittleEndian, ReadBytesExt};
 use std::convert::TryFrom;
 use std::error;
@@ -10,11 +10,11 @@ pub(crate) struct LinesDataReader<'a> {
 }
 
 impl LinesDataReader<'_> {
-    fn read_number_i32(&mut self) -> Result<i32, io::Error> {
+    fn read_i32(&mut self) -> Result<i32, io::Error> {
         self.file.read_i32::<LittleEndian>()
     }
 
-    fn read_number_f32(&mut self) -> Result<f32, io::Error> {
+    fn read_f32(&mut self) -> Result<f32, io::Error> {
         self.file.read_f32::<LittleEndian>()
     }
 
@@ -24,11 +24,10 @@ impl LinesDataReader<'_> {
         let num_pages = if self.version >= 3 {
             1
         } else {
-            self.read_number_i32()?
+            self.read_i32()?
         };
         (0..num_pages)
-            .map(|_p| {
-                println!("p: {} / {}", _p, num_pages);
+            .map(|_| {
                 Ok(Page {
                     layers: self.read_layers()?,
                 })
@@ -37,10 +36,9 @@ impl LinesDataReader<'_> {
     }
 
     fn read_layers(&mut self) -> Result<Vec<Layer>, Box<dyn error::Error>> {
-        let num_layers = self.read_number_i32()?;
+        let num_layers = self.read_i32()?;
         (0..num_layers)
-            .map(|_l| {
-                println!("l: {} / {}", _l, num_layers);
+            .map(|_| {
                 Ok(Layer {
                     lines: self.read_lines()?,
                 })
@@ -49,23 +47,18 @@ impl LinesDataReader<'_> {
     }
 
     fn read_lines(&mut self) -> Result<Vec<Line>, Box<dyn error::Error>> {
-        let num_lines = self.read_number_i32()?;
-        (0..num_lines)
-            .map(|_li| {
-                println!("li: {} / {}", _li, num_lines);
-                self.read_line()
-            })
-            .collect()
+        let num_lines = self.read_i32()?;
+        (0..num_lines).map(|_| self.read_line()).collect()
     }
 
     fn read_line(&mut self) -> Result<Line, Box<dyn error::Error>> {
         Ok(Line {
-            brush_type: BrushType::try_from(self.read_number_i32()?)?,
-            color: Color::try_from(self.read_number_i32()?)?,
-            unknown_line_attribute_1: self.read_number_i32()?,
-            brush_base_size: self.read_number_f32()?,
+            brush_type: BrushType::try_from(self.read_i32()?)?,
+            color: Color::try_from(self.read_i32()?)?,
+            unknown_line_attribute_1: self.read_i32()?,
+            brush_base_size: self.read_f32()?,
             unkonwn_line_attribute_2: if self.version >= 5 {
-                self.read_number_i32()?
+                self.read_i32()?
             } else {
                 0
             },
@@ -74,23 +67,18 @@ impl LinesDataReader<'_> {
     }
 
     fn read_points(&mut self) -> Result<Vec<Point>, io::Error> {
-        let num_points = self.read_number_i32()?;
-        (0..num_points)
-            .map(|_pt| {
-                println!("pt: {} / {}", _pt, num_points);
-                self.read_point()
-            })
-            .collect()
+        let num_points = self.read_i32()?;
+        (0..num_points).map(|_| self.read_point()).collect()
     }
 
     fn read_point(&mut self) -> Result<Point, io::Error> {
         Ok(Point {
-            x: self.read_number_f32()?,
-            y: self.read_number_f32()?,
-            speed: self.read_number_f32()?,
-            direction: self.read_number_f32()?,
-            width: self.read_number_f32()?,
-            pressure: self.read_number_f32()?,
+            x: self.read_f32()?,
+            y: self.read_f32()?,
+            speed: self.read_f32()?,
+            direction: self.read_f32()?,
+            width: self.read_f32()?,
+            pressure: self.read_f32()?,
         })
     }
 }
